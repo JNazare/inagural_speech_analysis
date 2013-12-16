85468ed3c8bf1e7fc2f2a333c886939e721943ad
Juliana Nazare <jules.nazare@gmail.com>
2013-12-16 17:02:12 -0500

    json

--- rerun_experiments.py
+++ rerun_experiments.py
@@ -6,7 +6,6 @@ from pprint import pprint
 import math
 import networkx as nx
 
-# globals
 filenames = inaugural.fileids()
 
 def dump_content(filename, content):
@@ -51,63 +50,66 @@ def process_speeches(filenames):
 	dump_content('initial_processing', texts)
 	return texts
 
-docs = read_content('initial_processing')
-print len(docs.keys())
+def get_buzzwords(docs):
+	buzzwords = []
+	for doc in docs:
+		freqdist = FreqDist(docs[doc])
+		vocab = freqdist.keys()
+		freqs = freqdist.values()
+		buzzwords = buzzwords + vocab[:50]
 
-# Got buzzwords ==> put this in a function
-# buzzwords = []
-# for doc in docs:
-# 	freqdist = FreqDist(docs[doc])
-# 	vocab = freqdist.keys()
-# 	freqs = freqdist.values()
-# 	buzzwords = buzzwords + vocab[:50]
+	buzzwords = set(buzzwords)
 
-# buzzwords = set(buzzwords)
-# # print buzzwords
+	freq_counts = {}
+	for buzzword in buzzwords:
+		print buzzword
+		l = []
+		for doc in docs:
+			freqdist = FreqDist(docs[doc])
+			t = (doc, freqdist[buzzword])
+			l.append(t)
+		freq_counts[buzzword] = l
+	dump_content('freqs', freq_counts)
+	return freq_counts
 
-# freq_counts = {}
-# for buzzword in buzzwords:
-# 	print buzzword
-# 	l = []
-# 	for doc in docs:
-# 		freqdist = FreqDist(docs[doc])
-# 		t = (doc, freqdist[buzzword])
-# 		l.append(t)
-# 	freq_counts[buzzword] = l
-# print freq_counts
-# dump_content('freqs', freq_counts)
-
+# docs = read_content('initial_processing')
 docs = read_content('freqs')
+
+# remove some random ascii chars that stuck around
 del docs[u'\ufffd\ufffd']
 del docs[u'\ufffd']
 
+# get the top two speeches per buzzword
 for doc in docs:
 	docs[doc] = sorted(docs[doc],key=lambda x: x[1], reverse=True)[:2]
+	docs[doc] = {docs[doc][0][0][:-4] : docs[doc][0][1], docs[doc][1][0][:-4] : docs[doc][1][1]}
 
-# Make list of edges
-for_edges = {}
-for doc in docs:
-	l = []
-	for name in docs[doc]:
-		l.append(name[0])
-	for_edges[doc]=l
+dump_content("top_buzzwords", docs)
 
-edge_dict = {}
-for doc in for_edges:
-	tuples = [(x,y) for x in for_edges[doc] for y in for_edges[doc] if x != y]
-	for entry in tuples:
-	    if (entry[1], entry[0]) in tuples:
-	        tuples.remove((entry[1],entry[0]))
-	edge_dict[doc] = tuples
+# # Make dictionary for edge processing
+# for_edges = {}
+# for doc in docs:
+# 	l = []
+# 	for name in docs[doc]:
+# 		l.append(name[0])
+# 	for_edges[doc]=l
 
-print len(docs.keys())
+# # make dictionary of edges between top two speeches for each buzzword
+# edge_dict = {}
+# for doc in for_edges:
+# 	tuples = [(x,y) for x in for_edges[doc] for y in for_edges[doc] if x != y]
+# 	for entry in tuples:
+# 	    if (entry[1], entry[0]) in tuples:
+# 	        tuples.remove((entry[1],entry[0]))
+# 	edge_dict[doc] = tuples
 
-# Make the graph
-G=nx.Graph()
-for doc in edge_dict:
-	G.add_edges_from(edge_dict[doc])
+# # Make the graph
+# G=nx.Graph()
+# for doc in edge_dict:
+# 	G.add_edges_from(edge_dict[doc])
 
-nx.write_gexf(G, 'speeches.gexf')
+# # export graph to gexf so it can be read into Gephi
+# nx.write_gexf(G, 'speeches.gexf')
 
 
 
